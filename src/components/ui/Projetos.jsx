a7e88fe9e6a7e86acee6d6e32637d66193da072d
Roger-sl-dev <rogeralxslv@hotmail.com>
2025-07-28 21:43:38 -0300

    Update Projetos.jsx

--- src/components/ui/Projetos.jsx
+++ src/components/ui/Projetos.jsx
@@ -17,7 +17,7 @@ export default({Foto,Title})=>{
             
                 >
 
-        <div className=' hover:w-[500px] cursor-pointer  transition-all duration-700 border border-white rounded-lg hover:border-lime-300  hover:border-2  w-[450px]'>
+        <div className='  cursor-pointer  transition-all duration-700 border border-white rounded-lg hover:border-lime-300  hover:border-2  w-[450px]'>
             <img className=' rounded-lg' src={Foto} alt="" />
             <p></p>
         </div>
